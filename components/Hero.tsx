03c1ab6e202371be9104faade3f91c87df721c31
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-06-26 11:52:38 -0500

    feat: add text effects and introduction to Hero component
    - Added TextGenerateEffect component with introductory phrase
    - Included a personal introduction paragraph in the Hero component
    - Changed heading text to 'Dynamic web magic with Next.js' for better clarity and impact

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -21,8 +22,17 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Edgar's Portfolio
+            Dynamic web magic with Next.js
           </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Edgar, a Full-Stack Developer based in Peru.
+          </p>
         </div>
       </div>
     </div>
