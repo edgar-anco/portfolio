dbcaf8b32f8618770f9a62ed7bb1e4dee02fc8b4
Edgar Andre Anco Copaja <89050053+edgar-anco@users.noreply.github.com>
2024-06-28 22:49:17 -0500

    feat: add MagicButton to Hero component
    - Added MagicButton component with title and icon
    - Changed TextGenerateEffect text to 'Transforming Concepts into Seamless User Experiences' for better clarity and impact

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "./ui/MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
@@ -27,12 +29,20 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Edgar, a Full-Stack Developer based in Peru.
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
